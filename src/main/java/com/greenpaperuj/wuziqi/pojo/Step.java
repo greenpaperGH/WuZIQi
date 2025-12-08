7b8e0b5f085cecbcec7c60d3b27b22e557a6bc37
olive77 <640409632@qq.com>
2025-12-08 16:55:11 +0800

    尝试对对象重命名

---
+++ src/main/java/com/greenpaperuj/wuziqi/pojo/Step.java
@@ -0,0 +1,11 @@
+package com.greenpaperuj.wuziqi.pojo;
+
+import lombok.Data;
+
+@Data
+public class Step {
+    private int x;
+    private int y;
+    private int color; // 0 for black, 1 for white
+    
+}
